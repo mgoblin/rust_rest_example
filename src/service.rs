d65fa52a56485174eacef49e78d81b289c63553d
Michael Golovanov <mike.golovanov@gmail.com>
2022-05-01 07:15:20 +0300

    UsersService.find_by_id impl

--- src/service.rs
+++ src/service.rs
@@ -26,4 +26,8 @@ impl UsersService {
 
         self.rb.fetch_page_by_wrapper(wraper,  req).await.unwrap()
     }
+
+    pub async fn find_by_id(&self, id: u64) -> std::option::Option<Users> {
+        self.rb.fetch_by_column("id", id).await.unwrap()
+    }
 }
