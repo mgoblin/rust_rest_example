73dc6b71ee5a958ea9f3afaa97fea09d4475c262
Michael Golovanov <mike.golovanov@gmail.com>
2022-05-18 10:40:23 +0300

    add Day 1.1 numbers example implemented

--- day1_1_numbers/src/main.rs
+++ day1_1_numbers/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    // Rust floats f64 precision traps
+    let x = 0.1 + 0.2;
+    let y: f32 = 0.1 + 0.2;
+    println!("{}", 0.1 + 0.2);
+    println!("{}", x);
+    println!("{}", y);
 }
