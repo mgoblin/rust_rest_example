203ac6f62dad7d8a47a9c34f1f76216cddb6d766
Michael Golovanov <mike.golovanov@gmail.com>
2022-05-20 16:26:34 +0300

    Module examples implmented

---
+++ day3_1_modules_decl/src/main.rs
@@ -0,0 +1,78 @@
+mod pets {
+    use std::fmt::Display;
+
+    #[derive(Debug, PartialEq, Hash)]
+    pub struct Dog {
+        name: String,
+    }
+
+    impl Dog {
+        pub fn new(name: &str) -> Dog {
+            Dog {
+                name: String::from(name),
+            }
+        }
+
+        pub fn name(&self) -> &str {
+            &self.name[..]
+        }
+    }
+
+    #[derive(Debug, Clone, Copy, PartialEq, Hash)]
+    pub enum Color {
+        Red, Green, Blue
+    }
+
+    impl Display for Color {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let color = match &self {
+                Color::Red => "Red",
+                Color::Green => "Green",
+                Color::Blue => "Blue"
+            };
+            write!(f, "{}", color)
+        }
+    }
+
+    #[derive(Debug, Clone, PartialEq, Hash)]
+    pub struct Fish {
+        color: Color,
+    } 
+
+    impl Fish {
+        pub fn new(c: Color) -> Fish {
+            Fish {
+                color: c
+            }
+        }
+
+        pub fn color(&self) -> Color {
+            self.color
+        }
+    }
+
+    pub trait Greeting {
+        fn greet(&self) -> String {
+            String::from("Silent pet")
+        }
+    }
+
+    impl Greeting for Dog {
+        fn greet(&self) -> String {
+            String::from("Gaw Gaw")
+        }
+    }
+
+    impl Greeting for Fish {}
+
+}
+
+use pets::{Dog, Fish, Color, Greeting};
+
+fn main() {
+    let dog = Dog::new("Lucky");
+    println!("Its a dog {} that say {}", dog.name(), dog.greet());
+
+    let fish = Fish::new(Color::Red);
+    println!("Its a fish {} that say {}", fish.color(), fish.greet());
+}
