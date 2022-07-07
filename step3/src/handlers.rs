908137eef9a767ae1c2c474931bb7112e141a172
Michael Golovanov <mike.golovanov@gmail.com>
2022-07-07 13:20:38 +0300

    http services tests implemented

--- step3/src/handlers.rs
+++ step3/src/handlers.rs
@@ -53,11 +53,13 @@ pub async fn delete_user(uid: web::Path<u64>, dao: Data<UserInMemoryDAO>) -> imp
 
 #[cfg(test)]
 mod tests {
-    use actix_web::{test, http};
-    use actix_web::web::Data;
 
-    use crate::configs::InMemory;
-    use crate::handlers::*;
+    use super::*;
+    use actix_web::http::StatusCode;
+    use actix_web::{test, http, App, body};
+    use actix_web::web::{Data, Bytes};
+
+    use crate::configs::{InMemory};
     use crate::services::UserInMemoryDAO;
 
     #[test]
@@ -69,6 +71,118 @@ mod tests {
 
         let resp = result.respond_to(req);
         assert_eq!(resp.status(), http::StatusCode::OK);
+    }
+
+    #[actix_web::test]
+    async fn test_get_user_by_id_found() {
+        let inmemory = InMemory {users: 1};
+
+        let user_dao = UserInMemoryDAO::new(Some(&inmemory));
+        let user_data = Data::new(user_dao); 
+
+        let app = test::init_service(
+            App::new()
+                .app_data(user_data)
+                .service(get_user_by_id),
+        ).await;
+
+        let req = test::TestRequest::get()
+            .uri("/users/1")
+            .to_request();
+        let user: User = test::call_and_read_body_json(&app, req).await;
+
+        assert_eq!(User {id: 1, name: "user 1".to_string()}, user);
+    }
+
+    #[actix_web::test]
+    async fn test_get_user_by_id_not_found() {
+        let inmemory = InMemory {users: 0};
+
+        let user_dao = UserInMemoryDAO::new(Some(&inmemory));
+        let user_data = Data::new(user_dao); 
+
+        let app = test::init_service(
+            App::new()
+                .app_data(user_data)
+                .service(get_user_by_id),
+        ).await;
+
+        let req = test::TestRequest::get()
+            .uri("/users/1")
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+        let status = resp.status();
+        let body_box = resp.into_body();
+        let body_bytes: Bytes = body::to_bytes(body_box).await.unwrap();
+        
+
+        assert_eq!(StatusCode::NOT_FOUND, status);
+        assert_eq!(true, body_bytes.is_empty());
+    }
+
+    #[actix_web::test]
+    async fn test_create_user() {
+        let inmemory = InMemory {users: 0};
+
+        let user_dao = UserInMemoryDAO::new(Some(&inmemory));
+        let user_data = Data::new(user_dao); 
+
+        let app = test::init_service(
+            App::new()
+                .app_data(user_data)
+                .service(create_user),
+        ).await;
+
+        let req = test::TestRequest::post()
+            .uri("/users")
+            .set_payload("user 1")
+            .to_request();
+        let user: User = test::call_and_read_body_json(&app, req).await;
+
+        assert_eq!(User {id: 1, name: "user 1".to_string()}, user);
+    }
+
+    #[actix_web::test]
+    async fn test_update_user() {
+        let inmemory = InMemory {users: 1};
+
+        let user_dao = UserInMemoryDAO::new(Some(&inmemory));
+        let user_data = Data::new(user_dao); 
+
+        let app = test::init_service(
+            App::new()
+                .app_data(user_data)
+                .service(update_user),
+        ).await;
+
+        let req = test::TestRequest::post()
+            .uri("/users/1")
+            .set_payload("user 2")
+            .to_request();
+        let user: User = test::call_and_read_body_json(&app, req).await;
+
+        assert_eq!(User {id: 1, name: "user 2".to_string()}, user);
+    }
+
+    #[actix_web::test]
+    async fn test_delete_user() {
+        let inmemory = InMemory {users: 1};
+
+        let user_dao = UserInMemoryDAO::new(Some(&inmemory));
+        let user_data = Data::new(user_dao); 
+
+        let app = test::init_service(
+            App::new()
+                .app_data(user_data)
+                .service(delete_user),
+        ).await;
+
+        let req = test::TestRequest::delete()
+            .uri("/users/1")
+            .to_request();
+        let user: User = test::call_and_read_body_json(&app, req).await;
+
+        assert_eq!(User {id: 1, name: "user 1".to_string()}, user);
     }
 }
 
